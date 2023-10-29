069afb96c3b7eb41cb872b7dee00284a30f942b1
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-29 23:49:45 +0200

    Included memory usage

--- ServerStatisticsCollector.cs
+++ ServerStatisticsCollector.cs
@@ -6,15 +6,19 @@ namespace ServerMonitoring
     {
         private PerformanceCounter _cpuCounter;
         private PerformanceCounter _ramCounter;
+        private Process _currentProcess;
 
         public ServerStatisticsCollector()
         {
             _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            _currentProcess = Process.GetCurrentProcess();
         }
 
         private double GetCurrentCpuUsage() => _cpuCounter.NextValue();
 
         private double GetCurrentAvailableMemory() => _ramCounter.NextValue();
+
+        private double GetCurrentMemoryUsage() => _currentProcess.WorkingSet64 / (1024 * 1024);
     }
 }
