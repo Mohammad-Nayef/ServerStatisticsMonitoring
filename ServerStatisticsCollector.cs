23ec0b6362437a47cec1e03145280327853f61f9
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-29 23:48:41 +0200

    Included the available memory

--- ServerStatisticsCollector.cs
+++ ServerStatisticsCollector.cs
@@ -5,12 +5,16 @@ namespace ServerMonitoring
     public class ServerStatisticsCollector
     {
         private PerformanceCounter _cpuCounter;
+        private PerformanceCounter _ramCounter;
 
         public ServerStatisticsCollector()
         {
             _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
         }
 
         private double GetCurrentCpuUsage() => _cpuCounter.NextValue();
+
+        private double GetCurrentAvailableMemory() => _ramCounter.NextValue();
     }
 }
