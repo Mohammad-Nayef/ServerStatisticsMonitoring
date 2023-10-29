22d1d6c5f7b6021b177f2fd8b85076d6fc6b08d7
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-30 00:03:48 +0200

    Created a property for the current server statistics

--- ServerStatisticsCollector.cs
+++ ServerStatisticsCollector.cs
@@ -4,12 +4,28 @@ namespace ServerMonitoring
 {
     public class ServerStatisticsCollector
     {
+        private ServerStatisticsDTO _currentServerStatistics;
         private PerformanceCounter _cpuCounter;
         private PerformanceCounter _ramCounter;
         private Process _currentProcess;
+        private bool _firstCall = true;
+
+        public ServerStatisticsDTO CurrentServerStatistics
+        {
+            get
+            {
+                _currentServerStatistics.Timestamp = DateTime.Now;
+                _currentServerStatistics.CpuUsage = GetCurrentCpuUsage();
+                _currentServerStatistics.AvailableMemory = GetCurrentAvailableMemory();
+                _currentServerStatistics.MemoryUsage = GetCurrentMemoryUsage();
+
+                return _currentServerStatistics;
+            }
+        }
 
         public ServerStatisticsCollector()
         {
+            _currentServerStatistics = new ServerStatisticsDTO();
             _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
             _currentProcess = Process.GetCurrentProcess();
@@ -19,6 +35,20 @@ namespace ServerMonitoring
 
         private double GetCurrentAvailableMemory() => _ramCounter.NextValue();
 
-        private double GetCurrentMemoryUsage() => _currentProcess.WorkingSet64 / (1024 * 1024);
+        private double GetCurrentMemoryUsage()
+        {
+            if (_firstCall)
+            {
+                _firstCall = false;
+            }
+            else
+            {
+                _currentProcess.Refresh();
+            }
+
+            return ToMegaBytes(_currentProcess.WorkingSet64);
+        }
+
+        private double ToMegaBytes(long workingSet64) => workingSet64 / (1024 * 1024);
     }
 }
