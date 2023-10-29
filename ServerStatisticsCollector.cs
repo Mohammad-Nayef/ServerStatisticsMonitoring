84e88fe6767b178cdcc860fe2bfdbf3766acab30
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-29 23:46:36 +0200

    Included the cpu usage

---
+++ ServerStatisticsCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ServerMonitoring
+{
+    public class ServerStatisticsCollector
+    {
+        private PerformanceCounter _cpuCounter;
+
+        public ServerStatisticsCollector()
+        {
+            _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+
+        private double GetCurrentCpuUsage() => _cpuCounter.NextValue();
+    }
+}
