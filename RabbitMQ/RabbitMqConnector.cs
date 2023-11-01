5dda22c3f2df6fa18fc65bb0156699703262cb27
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-01 23:20:34 +0200

    Added RabbitMQ to the solution

---
+++ RabbitMQ/RabbitMqConnector.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    public abstract class RabbitMqConnector
+    {
+        private ConnectionFactory _factory = new();
+        private IConnection _connection;
+        protected IModel _channel;
+
+        public RabbitMqConnector(string connectionName)
+        {
+            _factory.HostName = "localhost";
+            _factory.ClientProvidedName = connectionName;
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        ~RabbitMqConnector()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
+    }
+}
---
+++ RabbitMQ/TopicMessageSender.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RabbitMq
+{
+    public class TopicMessageSender : RabbitMqConnector
+    {
+        private string _exchangeName;
+        private string _routingKey;
+
+        public TopicMessageSender(string connectionName, string exchangeName,
+            string queueName, string routingKey) : base(connectionName)
+        {
+            _channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+            _channel.QueueDeclare(queueName, false, false, false);
+            _channel.QueueBind(queueName, exchangeName, routingKey);
+            _exchangeName = exchangeName;
+            _routingKey = routingKey;
+        }
+
+        public void Publish<T>(T message)
+        {
+            var stringMessage = JsonSerializer.Serialize(message);
+            var bytesMessage = Encoding.UTF8.GetBytes(stringMessage);
+            _channel.BasicPublish(_exchangeName, _routingKey, null, bytesMessage);
+        }
+    }
+}
