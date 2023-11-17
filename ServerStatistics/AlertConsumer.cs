deaa07755ada5f1275e9574f6af4b7eb796a5397
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-17 22:19:23 +0200

    Added an alert consumer

---
+++ ServerStatistics/AlertConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ServerStatistics
+{
+    public class AlertConsumer
+    {
+        public async Task ConsumeAsync()
+        {
+            var hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7265/Alerts")
+                .Build();
+
+            hubConnection.On<string>("ReceiveAlerts", alert =>
+            {
+                Console.WriteLine(alert);
+            });
+
+            await hubConnection.StartAsync();
+        }
+    }
+}
