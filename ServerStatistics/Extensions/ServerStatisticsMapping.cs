f43113f0bc6078a82b9344e9bce85fa38f4b1d58
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-19 11:32:12 +0200

    Created an extension class for mapping server statistics models

---
+++ ServerStatistics/Extensions/ServerStatisticsMapping.cs
@@ -0,0 +1,20 @@
+﻿using ServerStatistics.Models;
+
+namespace ServerStatistics.Extensions
+{
+    public static class ServerStatisticsMapping
+    {
+        public static ServerStatisticsWithServerIdentifierDTO IncludeServerIdentifier(
+            this ServerStatisticsDTO serverStatistics, string serverIdentifier)
+        {
+            return new ServerStatisticsWithServerIdentifierDTO
+            {
+                AvailableMemory = serverStatistics.AvailableMemory,
+                CpuUsage = serverStatistics.CpuUsage,
+                MemoryUsage = serverStatistics.MemoryUsage,
+                Timestamp = serverStatistics.Timestamp,
+                ServerIdentifier = serverIdentifier
+            };
+        }
+    }
+}
