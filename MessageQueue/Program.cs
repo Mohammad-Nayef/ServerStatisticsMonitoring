7247334efd7c48eeaeb15e502c9335d558c68c70
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-19 12:30:57 +0200

    Added the main method of the project

---
+++ MessageQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using MessageQueue;
+using ServerStatistics.Services;
+using ServerStatistics.Repositories;
+using SignalREndpoint;
+
+Console.WriteLine(
+    "This service is responsible for consuming server statistics from the message " +
+    "queue and sending anomaly alerts through SignalR\n");
+
+var messageConsumer = new ServerStatisticsReceiver(
+        "Message consuming",
+        "Statistics exchange",
+        "ServerStatistics",
+        "ServerStatistics.*",
+        new ServerStatisticsService(new MongoDbServerStatisticsRepository()),
+        new SignalRAlertSender()
+        );
+
+messageConsumer.Consume();
+
+Console.ReadLine();
