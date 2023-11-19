d3e0541c05423e8bcea578a5bc9815db2b6c2d0c
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-19 12:26:34 +0200

    Created an abstraction for RabbitMQ connection

---
+++ MessageQueue/RabbitMqConnector.cs
@@ -0,0 +1,27 @@
+﻿using GlobalConfigurations;
+using RabbitMQ.Client;
+
+namespace MessageQueue
+{
+    public abstract class RabbitMqConnector
+    {
+        private AppConfigurations _config = new();
+        private ConnectionFactory _factory = new();
+        private IConnection _connection;
+        protected IModel _channel;
+
+        public RabbitMqConnector(string connectionName)
+        {
+            _factory.HostName = _config.RabbitMqConfig.HostName;
+            _factory.ClientProvidedName = connectionName;
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        ~RabbitMqConnector()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
+    }
+}
