9025576a71334ea4e05d427f8179bb010142ca5f
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-19 12:27:47 +0200

    Created a class to send messages through a message queue by topic

---
+++ MessageQueue/TopicMessageSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace MessageQueue
+{
+    public class TopicMessageSender : RabbitMqConnector
+    {
+        private string _exchangeName;
+        private string _routingKey;
+
+        public TopicMessageSender(string connectionName, string exchangeName,
+            string queueName, string routingKey) : base(connectionName)
+        {
+            _channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+            _channel.QueueDeclare(queueName, false, false, false);
+            _channel.QueueBind(queueName, exchangeName, routingKey);
+            _exchangeName = exchangeName;
+            _routingKey = routingKey;
+        }
+
+        public void Send<T>(T message)
+        {
+            var stringMessage = JsonSerializer.Serialize(message);
+            var bytesMessage = Encoding.UTF8.GetBytes(stringMessage);
+            _channel.BasicPublish(_exchangeName, _routingKey, null, bytesMessage);
+        }
+    }
+}
