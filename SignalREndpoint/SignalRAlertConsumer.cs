0c8b586fd3da87a452fdf2470e475118d23b9fd8
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-22 13:03:43 +0200

    Moved SignalR sender and consumer to SignalREndpoint

---
+++ SignalREndpoint/SignalRAlertConsumer.cs
@@ -0,0 +1,34 @@
+﻿using GlobalConfigurations;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalREndpoint
+{
+    public class SignalRAlertConsumer : IAlertConsumer
+    {
+        public HubConnection Connection { get; private set; }
+
+        public SignalRAlertConsumer()
+        {
+            var config = new AppConfigurations();
+
+            Connection = new HubConnectionBuilder()
+                .WithUrl(config.SignalRConfig.Url)
+                .Build();
+        }
+
+        public async Task ConsumeAsync()
+        {
+            Connection.On<string>("ReceiveAlerts", alert =>
+            {
+                Console.WriteLine(alert);
+            });
+
+            await Connection.StartAsync();
+        }
+
+        public async Task ReconnectAsync()
+        {
+            await Connection.StartAsync();
+        }
+    }
+}
---
+++ SignalREndpoint/SignalRAlertSender.cs
@@ -0,0 +1,28 @@
+﻿using GlobalConfigurations;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalREndpoint
+{
+    public class SignalRAlertSender : IAlertSender
+    {
+        private HubConnection? _hubConnection;
+
+        public SignalRAlertSender()
+        {
+            var config = new AppConfigurations();
+
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(config.SignalRConfig.Url)
+                .Build();
+        }
+
+        public async Task SendAsync(string alert)
+        {
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<string>("SendMessageAsync", alert);
+
+            await _hubConnection.StopAsync();
+        }
+    }
+}
