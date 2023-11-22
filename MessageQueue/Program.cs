193e80b8c07d15db7113b6352e8e2fc7171c3754
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-22 13:28:37 +0200

    Reconnecting of connection failed

--- MessageQueue/Program.cs
+++ MessageQueue/Program.cs
@@ -7,15 +7,34 @@ Console.WriteLine(
     "This service is responsible for consuming server statistics from the message " +
     "queue and sending anomaly alerts through SignalR\n");
 
-var messageConsumer = new ServerStatisticsReceiver(
-        "Message consuming",
-        "Statistics exchange",
-        "ServerStatistics",
-        "ServerStatistics.*",
-        new ServerStatisticsService(new MongoDbServerStatisticsRepository()),
-        new SignalRAlertSender()
-        );
+Console.WriteLine("Connecting...");
 
-messageConsumer.Consume();
+while (true)
+{
+    try
+    {
+        var messageConsumer = new ServerStatisticsReceiver(
+                "Message consuming",
+                "Statistics exchange",
+                "ServerStatistics",
+                "ServerStatistics.*",
+                new ServerStatisticsService(new MongoDbServerStatisticsRepository()),
+                new SignalRAlertSender()
+                );
 
-Console.ReadLine();
+        messageConsumer.Consume();
+
+        Console.WriteLine("Connected successfully.");
+        Console.ReadLine();
+    }
+    catch
+    {
+        Console.WriteLine("\nConnection to RabbitMQ failed!");
+        Thread.Sleep(2000);
+        Console.WriteLine("Reconnecting...");
+
+        continue;
+    }
+
+    break;
+}
