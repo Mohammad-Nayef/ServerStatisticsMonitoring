3d75074dd5a9fd93582b4ae6d9690cec9a905342
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-22 13:26:01 +0200

    Added main method for consuming alerts

---
+++ ServerStatistics/Program.cs
@@ -0,0 +1,35 @@
+﻿using SignalREndpoint;
+
+Console.WriteLine("This service is responsible for consuming alerts from SignalR and printing anomalies alerts.\n");
+
+Console.WriteLine("Connecting...");
+
+SignalRAlertConsumer? alertConsumer = null;
+
+while (true)
+{
+    try
+    {
+        alertConsumer = new SignalRAlertConsumer();
+        await alertConsumer.ConsumeAsync();
+    }
+    catch
+    {
+        Console.WriteLine("\nConnection to SignalR failed!");
+        Thread.Sleep(2000);
+        Console.WriteLine("Reconnecting...");
+
+        continue;
+    }
+
+    break;
+}
+
+alertConsumer.Connection.Closed += async (args) => 
+{
+    Console.WriteLine("Disconnected!");
+    await alertConsumer.ReconnectAsync();
+};
+
+Console.WriteLine("Connected successfully\n");
+Console.ReadLine();
