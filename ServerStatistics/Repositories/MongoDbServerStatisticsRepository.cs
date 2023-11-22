8298e422e97dbac798b003ed538205f4a7dae046
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-22 13:16:24 +0200

    Created a repository for server statistics

---
+++ ServerStatistics/Repositories/MongoDbServerStatisticsRepository.cs
@@ -0,0 +1,28 @@
+﻿using GlobalConfigurations;
+using MongoDB.Driver;
+using ServerStatistics.Models;
+
+namespace ServerStatistics.Repositories
+{
+    public class MongoDbServerStatisticsRepository : IServerStatisticsRepository
+    {
+        private AppConfigurations _config = new();
+        private MongoClient _client;
+        private IMongoDatabase _database;
+        private IMongoCollection<ServerStatisticsWithServerIdentifierDTO> _serverStatistics;
+
+        public MongoDbServerStatisticsRepository()
+        {
+            _client = new MongoClient(_config.MongoDbConfig.ConnectionString);
+            _database = _client.GetDatabase("ServerMonitoring");
+
+            _serverStatistics = _database.
+                GetCollection<ServerStatisticsWithServerIdentifierDTO>("ServerStatistics");
+        }
+
+        public async Task InsertAsync(ServerStatisticsWithServerIdentifierDTO serverStatistics)
+        {
+            await _serverStatistics.InsertOneAsync(serverStatistics);
+        }
+    }
+}
