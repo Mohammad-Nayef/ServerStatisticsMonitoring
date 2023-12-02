7cbdcc85056bf6bcab504c117073c9381ef7a4c5
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-02 21:27:03 +0200

    Overrode ToString()

--- ServerStatistics/Models/ServerStatisticsDTO.cs
+++ ServerStatistics/Models/ServerStatisticsDTO.cs
@@ -6,5 +6,13 @@
         public double AvailableMemory { get; set; }
         public double CpuUsage { get; set; }
         public DateTime Timestamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"\nMemory Usage: {MemoryUsage * 100} %\n" +
+                   $"Available Memory: {AvailableMemory}\n" +
+                   $"CPU Usage: {CpuUsage * 100} %\n" +
+                   $"Timestamp: {Timestamp}\n";
+        }
     }
 }
